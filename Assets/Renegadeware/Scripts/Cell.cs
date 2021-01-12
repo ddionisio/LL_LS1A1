405244c023122912d0095d7d7c0873da2ab81a62
David Dionisio <ddionisio@renegadeware.com>
2021-01-12 17:11:32 -0500

    separate persistent settings and user data, some thoughts/planning on cell stuff

--- Assets/Renegadeware/Scripts/Cell.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace Renegadeware.LL_LS1A1 {
-    public class Cell : MonoBehaviour {
-    }
-}
--- Assets/Renegadeware/Scripts/CellEntity.cs
+++ Assets/Renegadeware/Scripts/CellEntity.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 namespace Renegadeware.LL_LS1A1 {
     public class CellEntity : MonoBehaviour {
+
+        /// <summary>
+        /// Call this after creating the prefab, before generating the pool.
+        /// </summary>
+        /// <param name="cellTemplate"></param>
+        public void SetupTemplate(CellTemplate cellTemplate) {
+            
+        }
+
+        void Awake() {
+            
+        }
     }
 }
