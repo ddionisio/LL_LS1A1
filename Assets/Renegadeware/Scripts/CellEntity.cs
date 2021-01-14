5ace34e936d195fbc3185dedde4b5b695ac34e08
David Dionisio <ddionisio@renegadeware.com>
2021-01-14 18:21:31 -0500

    some data structure considerations

--- Assets/Renegadeware/Scripts/CellEntity.cs
+++ Assets/Renegadeware/Scripts/CellEntity.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 namespace Renegadeware.LL_LS1A1 {
     public class CellEntity : MonoBehaviour {
 
+        public Rigidbody2D body { get { return mBody; } }
+        public M8.SpriteColorGroup color { get { return mColor; } }
+
+        private Rigidbody2D mBody;
+        private M8.SpriteColorGroup mColor;
+
         /// <summary>
         /// Call this after creating the prefab, before generating the pool.
         /// </summary>
         /// <param name="cellTemplate"></param>
         public void SetupTemplate(CellTemplate cellTemplate) {
-            
+            //go through and initialize organelles
         }
 
         void Awake() {
+            mBody = GetComponent<Rigidbody2D>();
+            mColor = GetComponent<M8.SpriteColorGroup>();
+        }
+
+
+
+        void OnTriggerEnter2D(Collider2D collision) {
+            
+        }
+
+        void OnTriggerExit2D(Collider2D collision) {
             
         }
     }
--- Assets/Renegadeware/Scripts/Organelles/Organelle.cs
+++ Assets/Renegadeware/Scripts/Organelles/Organelle.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Renegadeware.LL_LS1A1 {
-    [CreateAssetMenu(fileName = "organelle", menuName = "Game/Organelle")]
-    public class OrganelleData : ScriptableObject {
+    public abstract class Organelle : ScriptableObject {
         public int id;
         public GameObject prefab;
 
-        [Header("Info Data")]
+        [Header("Info")]
         public Sprite icon;
         [M8.Localize]
         public string nameRef;
         [M8.Localize]
         public string descRef;
 
-        [Header("Game Data")]        
+        [Header("General")]
+        public float energy; //initial energy value upon spawn
         public float energyRate; //determines amount of energy consumption/regeneration
-        public float mass; //determines energy capacity for growth
+        public float energyCapacity; //determines energy cap, when reached, allow division
     }
 }
