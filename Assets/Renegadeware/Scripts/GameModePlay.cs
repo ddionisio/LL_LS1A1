a595f6c8c5cfa036e0fc0c6589bb45a5abd26618
David Dionisio <ddionisio@renegadeware.com>
2021-02-01 17:11:43 -0500

    more gameflow stuff, hud interfaces

--- Assets/Renegadeware/Scripts/GameModePlay.cs
+++ Assets/Renegadeware/Scripts/GameModePlay.cs
@@ -9,22 +9,48 @@ namespace Renegadeware.LL_LS1A1 {
         [Header("Level Data")]
         public LevelData level;
 
+        protected override void OnInstanceDeinit() {
+            HUD.instance.HideAll();
+
+            base.OnInstanceDeinit();
+        }
+
+        protected override void OnInstanceInit() {
+            base.OnInstanceInit();
+
+            //initialize some data
+        }
+
         protected override IEnumerator Start() {
             //Loading
             yield return base.Start();
 
             //game flow
+            StartCoroutine(DoEnvironmentSelect());
         }
 
         IEnumerator DoEnvironmentSelect() {
+            //open environment modal
+
+            //setup hud
+
+            //close environment modal
+
             yield return null;
         }
 
         IEnumerator DoOrganismEdit() {
+            //open organism modal
+
+            //setup hud
+
+            //close organism modal
+
             yield return null;
         }
 
         IEnumerator DoSimulation() {
+            //setup hud
             yield return null;
         }
     }
--- Assets/Renegadeware/Scripts/UI/HUD.cs
+++ Assets/Renegadeware/Scripts/UI/HUD.cs
@@ -7,31 +7,158 @@ using LoLExt;
 
 namespace Renegadeware.LL_LS1A1 {
     public class HUD : M8.SingletonBehaviour<HUD> {
+        public enum Element {
+            ModeSelect,
+            Gameplay
+        }
+
+        public enum ModeSelect {
+            Environment,
+            Edit,
+            Play
+        }
+
         [System.Flags]
-        public enum GameModeFlags {
+        public enum ModeSelectFlags {
             None = 0x0,
             Environment = 0x1,
             Edit = 0x2,
-            Simulate = 0x4
+            Play = 0x4
+        }
+
+        public struct TransitionQueue {
+            public Element elem;
+            public bool isEnter;
         }
 
         [Header("Mode")]
-        public GameObject gameModeRootGO;
-        public AnimatorEnterExit gameModeTransition;
-        public Button[] gameModeButtons; //corresponds to GameMode flags
+        public GameObject modeSelectRootGO;
+        public AnimatorEnterExit modeSelectTransition;
+        public Button[] modeSelectButtons; //corresponds to GameMode
 
-        public event System.Action<GameModeFlags> gameModeClickCallback;
+        [Header("Gameplay")]
+        public GameObject gameplayRootGO;
+        public AnimatorEnterExit gameplayTransition;
 
+        public bool isBusy { get { return mRout != null; } }
+
+        public event System.Action<ModeSelect> modeSelectClickCallback;
+
+        private M8.CacheList<TransitionQueue> mTransitionQueue = new M8.CacheList<TransitionQueue>(4);
+
+        private ModeSelectFlags mModeSelectFlags = ModeSelectFlags.None;
         private Coroutine mRout;
+
+        public bool ElementIsVisible(Element elem) {
+            switch(elem) {
+                case Element.ModeSelect:
+                    return modeSelectRootGO && modeSelectRootGO.activeSelf;
+                case Element.Gameplay:
+                    return gameplayRootGO && gameplayRootGO.activeSelf;
+                default:
+                    return false;
+            }
+        }
+
+        public void ElementShow(Element elem) {
+            mTransitionQueue.Add(new TransitionQueue { elem = elem, isEnter = true });
+
+            if(mRout == null)
+                mRout = StartCoroutine(DoTransitions());
+        }
+
+        public void ElementHide(Element elem) {
+            mTransitionQueue.Add(new TransitionQueue { elem = elem, isEnter = false });
+
+            if(mRout == null)
+                mRout = StartCoroutine(DoTransitions());
+        }
+
+        public void ModeSelectSetVisible(ModeSelectFlags flags) {
+            mModeSelectFlags = flags;
+            ApplyModeSelectVisible();
+        }
+
+        public void HideAll() {
+            if(modeSelectRootGO) modeSelectRootGO.SetActive(false);
+            if(gameplayRootGO) gameplayRootGO.SetActive(false);
+
+            mModeSelectFlags = ModeSelectFlags.None;
+            ApplyModeSelectVisible();
+
+            mTransitionQueue.Clear();
+
+            if(mRout != null) {
+                StopCoroutine(mRout);
+                mRout = null;
+            }
+        }
 
         void Awake() {
             HideAll();
 
             //hook up calls
+            for(int i = 0; i < modeSelectButtons.Length; i++) {
+                var mode = (ModeSelect)i;
+                modeSelectButtons[i].onClick.AddListener(delegate () { modeSelectClickCallback.Invoke(mode); });
+            }
         }
 
-        private void HideAll() {
+        IEnumerator DoTransitions() {
+            while(mTransitionQueue.Count > 0) {
+                var itm = mTransitionQueue.Remove();
 
+                GameObject rootGO = null;
+                AnimatorEnterExit trans = null;
+
+                switch(itm.elem) {
+                    case Element.ModeSelect:
+                        rootGO = modeSelectRootGO;
+                        trans = modeSelectTransition;
+                        break;
+
+                    case Element.Gameplay:
+                        rootGO = gameplayRootGO;
+                        trans = gameplayTransition;
+                        break;
+                }
+
+                if(itm.isEnter) {
+                    if(rootGO) rootGO.SetActive(true);
+
+                    if(trans) yield return trans.PlayEnterWait();
+                }
+                else {
+                    if(trans) yield return trans.PlayExitWait();
+
+                    if(rootGO) rootGO.SetActive(false);
+                }
+            }
+
+            mRout = null;
+        }
+
+        private void ApplyModeSelectVisible() {
+            for(int i = 0; i < modeSelectButtons.Length; i++) {
+                var go = modeSelectButtons[i].gameObject;
+
+                var mode = (ModeSelect)i;
+                var flag = ModeSelectFlags.None;
+
+                switch(mode) {
+                    case ModeSelect.Environment:
+                        flag = ModeSelectFlags.Edit;
+                        break;
+                    case ModeSelect.Edit:
+                        flag = ModeSelectFlags.Environment;
+                        break;
+                    case ModeSelect.Play:
+                        flag = ModeSelectFlags.Play;
+                        break;
+                }
+
+                go.SetActive((mModeSelectFlags & flag) == flag);
+            }
         }
     }
 }
